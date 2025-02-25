2b3b8efd5acaa923f42e522898b94e9c4ab4c497
kasman9 <158018212+kasman9@users.noreply.github.com>
2025-02-25 21:14:07 +0530

    Update APIRunner.java

--- src/test/java/qumu/runners/APIRunner.java
+++ src/test/java/qumu/runners/APIRunner.java
@@ -5,8 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = {"src/test/java/qumu/features/API-Test.feature"},
-        //glue = {"qumu.stepDefinitions", "qumu.hook"},
-        glue = {"qumu.stepDefinitions"/*,"qumu.hook"*/},
+        glue = {"qumu.stepDefinitions", "qumu.hook"},
         plugin = {
                 "pretty",
                 "html:test-output/cucumber-reports/cucumber-pretty-api",
